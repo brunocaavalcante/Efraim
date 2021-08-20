740b97186b3fea1bf1ef609080728dd775c3211c
Bruno Cavalcante <brunocavalcantedsilva@gmail.com>
2021-08-20 11:55:50 -0300

    #21 Criar tela Index modulo de Departamento

--- src/Web/Controllers/DepartamentoController.cs
+++ src/Web/Controllers/DepartamentoController.cs
@@ -1,15 +1,30 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
 using Business.Core.Intefaces;
 using Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models;
 
 namespace Web.Controllers
 {
     public class DepartamentoController : BaseController
     {
         private readonly IDepartamentoService service;
-        public DepartamentoController(IDepartamentoService _service, 
+        private readonly IMapper mapper;
+        public DepartamentoController(IDepartamentoService _service,
+                                      IMapper _mapper, 
                                       INotificador notificador) : base(notificador)
         {
             service = _service;
+            mapper = _mapper;
         }
+
+        public async Task<IActionResult> Index()
+        {            
+            //var lista = mapper.Map<List<DepartamentoViewModel>>(await service.Listar());
+            return View(new List<DepartamentoViewModel>());
+        }
+
     }
 }
---
+++ src/Web/Models/DepartamentoViewModel.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace Web.Models
+{
+    public class DepartamentoViewModel
+    {
+        public string Id { get; set; }
+        
+        public string Nome{ get; set;}
+
+        public string Descricao {get;set;}
+
+        public List<MembroViewModel> Membros{ get; set; }
+
+        public List<MembroViewModel> Lider{ get; set; }
+
+        public DateTime DataCadastro { get; set; }
+    }
+}
